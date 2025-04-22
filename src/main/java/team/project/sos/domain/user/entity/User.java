f77c7aeea6b3c60685715cf6d37a19b274e398b7
gganb <gksql0801@naver.com>
2025-04-22 21:59:24 +0900

    Feat/user : User 엔티티 작성 - BaseEntity 상속 - User Table 연결 - isDeleted 기본값 false 설정

--- src/main/java/team/project/sos/domain/user/entity/User.java
+++ src/main/java/team/project/sos/domain/user/entity/User.java
@@ -1,16 +1,41 @@
 package team.project.sos.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.project.sos.domain.user.enums.Grade;
+import team.project.sos.domain.user.enums.UserRole;
 
 @Entity
+@Table(name = "User")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+@Getter
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
 }
