4472a616abe2889619c42196678bc9a2ddc99d54
rang <withghd@gmail.com>
2025-04-22 23:08:46 +0900

    feat(store): Entity 생성

--- src/main/java/team/project/sos/domain/store/entity/Store.java
+++ src/main/java/team/project/sos/domain/store/entity/Store.java
@@ -1,4 +1,45 @@
 package team.project.sos.domain.store.entity;
 
-public class Store {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.project.sos.common.config.BaseTimeEntity;
+import team.project.sos.domain.user.entity.User;
+
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "store")
+public class Store extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "open_time", nullable = false)
+    private LocalTime openTime;
+
+    @Column(name = "close_time", nullable = false)
+    private LocalTime closeTime;
+
+    @Column(name = "min_order_price", nullable = false)
+    private int minOrderPrice = 0;
+
+    @Column(nullable = false)
+    private String status;
+
+    private String notice;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public void updateNotice(String notice) {
+        this.notice = notice;
+    }
 }
