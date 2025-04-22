5f0b7c2fbaaca2b120f732c023d50356e7dbeb21
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-22 19:45:03 +0900

    feat(order): 엔티티 및 기타 파일 생성

--- src/main/java/team/project/sos/domain/order/entity/Order.java
+++ src/main/java/team/project/sos/domain/order/entity/Order.java
@@ -1,11 +1,16 @@
 package team.project.sos.domain.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.project.sos.domain.menu.entity.Menu;
+import team.project.sos.domain.order.enums.OrderStatus;
+import team.project.sos.domain.store.entity.Store;
+import team.project.sos.domain.user.entity.User;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -16,5 +21,26 @@ public class Order {
 
     @Id
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // 외래키 컬럼 지정
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id") // 외래키 컬럼 지정
+    private Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id") // 외래키 컬럼 지정
+    private Menu menu;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private int price;
+
+    private LocalDateTime requestedAt;
+
+    private LocalDateTime createdAt;
 
 }
