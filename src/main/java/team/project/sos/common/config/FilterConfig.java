8c322f22ea56d1c1d48113cacfaf107694d4d183
gganb <gksql0801@naver.com>
2025-04-23 19:31:13 +0900

    Feat/auth : jwt 필터를 등록하는 config 생성

--- src/main/java/team/project/sos/common/config/FilterConfig.java
+++ src/main/java/team/project/sos/common/config/FilterConfig.java
@@ -2,13 +2,23 @@ package team.project.sos.common.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
 
-    private final JwtUtil jwtUtil;
+    private final JwtProvider jwtProvider;
 
+    @Bean
+    public FilterRegistrationBean<JwtAuthFilter> jwtFilterFilterRegistrationBean() {
+        FilterRegistrationBean<JwtAuthFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtAuthFilter(jwtProvider));
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
+    }
 
 }
