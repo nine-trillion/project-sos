edc18db4286fa9ce7d13d43dc6d4c963f0bcae88
gganb <gksql0801@naver.com>
2025-04-23 13:07:04 +0900

    Fix/user : @Service 위치 변경

--- src/main/java/team/project/sos/domain/auth/service/AuthService.java
+++ src/main/java/team/project/sos/domain/auth/service/AuthService.java
@@ -1,7 +1,6 @@
 package team.project.sos.domain.auth.service;
 
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface AuthService {
 }
--- src/main/java/team/project/sos/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/team/project/sos/domain/auth/service/AuthServiceImpl.java
@@ -1,4 +1,7 @@
 package team.project.sos.domain.auth.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthServiceImpl implements AuthService{
 }
