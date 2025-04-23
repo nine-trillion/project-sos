69ecc61b4b7bfcb61f57617812eb8cd1ba1af82d
yigani <144660094+yigani@users.noreply.github.com>
2025-04-23 16:03:12 +0900

    Update MenuService.java

--- src/main/java/team/project/sos/domain/menu/service/MenuService.java
+++ src/main/java/team/project/sos/domain/menu/service/MenuService.java
@@ -4,6 +4,7 @@ package team.project.sos.domain.menu.service;
 import team.project.sos.domain.menu.dto.request.CreateMenuRequestDto;
 import team.project.sos.domain.menu.dto.request.UpdateMenuRequestDto;
 import team.project.sos.domain.menu.dto.response.MenuResponseDto;
+import team.project.sos.domain.menu.entity.Menu;
 
 public interface MenuService {
     MenuResponseDto save(CreateMenuRequestDto requestDto);
@@ -11,4 +12,5 @@ public interface MenuService {
     MenuResponseDto update(Long menuId, UpdateMenuRequestDto requestDto);
     void remove(Long menuId);
     team.project.sos.domain.menu.entity.Menu findByIdOrElseThrow(Long menuId);
+    Menu findMenuIncludeDeleted(Long menuId);
 }
