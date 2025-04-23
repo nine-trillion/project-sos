662699a0ec18b3bea4b0e3b1639938c05e2429da
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-23 15:14:41 +0900

    feat(order): 주문 상태 기본값 지정 로직 추가

--- src/main/java/team/project/sos/domain/order/entity/Order.java
+++ src/main/java/team/project/sos/domain/order/entity/Order.java
@@ -15,6 +15,7 @@ import team.project.sos.domain.store.entity.Store;
 import team.project.sos.domain.user.entity.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -35,27 +36,38 @@ public class Order {
     @JoinColumn(name = "store_id") // 외래키 컬럼 지정
     private Store store;
 
-    @ManyToOne
-    @JoinColumn(name = "menu_id") // 외래키 컬럼 지정
-    private Menu menu;
-
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     private int price;
 
+    @CreationTimestamp
     private LocalDateTime requestedAt;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    /**
+     * INSERT 전에 자동 실행되는 메서드입니다.
+     * 주문 상태 기본값을 PENDING으로 설정합니다.
+     */
+    @PrePersist
+    public void prePersist() {
+        if (this.status == null) {
+            this.status = OrderStatus.PENDING;
+        }
+    }
+
+    /**
+     * 주문을 취소하기 위해 서비스에서 호출하는 메서드입니다.
+     */
     public void cancel() {
-        // 이미 취소된 주문
+        // 이미 취소된 주문인 경우 예외 발생
         if (this.status == OrderStatus.CANCELLED) {
             throw new OrderException(OrderError.ALREADY_CANCELLED);
         }
 
-        // 이미 조리가 시작되거나 완료된 주문
+        // 이미 조리가 시작되거나 완료된 주문인 경우 예외 발생
         if (this.status == OrderStatus.COOKING || this.status == OrderStatus.COMPLETED) {
             throw new OrderException(OrderError.ALREADY_COOKING);
         }
