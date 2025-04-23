37ee54bf702098ca65c63ff41f9c11364e620384
rang <withghd@gmail.com>
2025-04-23 20:09:47 +0900

    feat(store): 가게 CRUD 구현

--- src/main/java/team/project/sos/domain/store/dto/response/StoreDetailResponse.java
+++ src/main/java/team/project/sos/domain/store/dto/response/StoreDetailResponse.java
@@ -3,7 +3,6 @@ package team.project.sos.domain.store.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import team.project.sos.domain.menu.dto.response.MenuResponse;
-import team.project.sos.domain.menu.dto.response.MenuResponse;
 
 import java.util.List;
 
@@ -11,6 +10,10 @@ import java.util.List;
 @AllArgsConstructor
 public class StoreDetailResponse {
 
-    private final StoreSaveResponse store;
+    private final StoreResponse store;
     private final List<MenuResponse> menus;
+
+    public static StoreDetailResponse from(StoreResponse store, List<MenuResponse> menus) {
+        return new StoreDetailResponse(store, menus);
+    }
 }
--- src/main/java/team/project/sos/domain/store/repository/StoreRepository.java
+++ src/main/java/team/project/sos/domain/store/repository/StoreRepository.java
@@ -1,6 +1,8 @@
 package team.project.sos.domain.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import team.project.sos.domain.store.entity.Store;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     List<Store> findAllByNameContains(String name);
+
+    @Query("select s from Store s where s.owner.id = :ownerId")
+    List<Store> findStoresByOwner(@Param("ownerId") Long ownerId);
 }
