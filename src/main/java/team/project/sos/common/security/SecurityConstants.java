4444409c4937f1f74ccf7cd4e947a4ca67005dc3
gganb <gksql0801@naver.com>
2025-04-24 13:33:34 +0900

    Feat/auth : white list 추가 - "/api/auth/signup/owner"

--- src/main/java/team/project/sos/common/security/SecurityConstants.java
+++ src/main/java/team/project/sos/common/security/SecurityConstants.java
@@ -4,6 +4,7 @@ public class SecurityConstants {
     public static final String[] WHITE_LIST = {
             "/api/auth/signup",
             "/api/auth/admin/signup",
-            "/api/auth/login"
+            "/api/auth/login",
+            "/api/auth/signup/owner"
     };
 }
