a721da0b5feca5e4f329ab51c5464c8d4bfd14f7
gganb <gksql0801@naver.com>
2025-04-24 13:36:48 +0900

    Feat /user : 회원정보 수정 로직 추가 - 핸드폰 번호 중복 예외

--- src/main/java/team/project/sos/domain/user/service/UserServiceImpl.java
+++ src/main/java/team/project/sos/domain/user/service/UserServiceImpl.java
@@ -2,11 +2,14 @@ package team.project.sos.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.project.sos.common.excepion.BaseException;
+import team.project.sos.domain.user.dto.request.UserUpdateRequestDto;
 import team.project.sos.domain.user.dto.response.UserResponseDto;
 import team.project.sos.domain.user.entity.User;
-import team.project.sos.domain.user.exception.UserError;
 import team.project.sos.domain.user.repository.UserRepository;
+
+import static team.project.sos.domain.user.exception.UserError.*;
 
 @Service
 @RequiredArgsConstructor
@@ -18,13 +21,25 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByIdOrElseThrow(Long userId) {
         return userRepository.findById(userId).orElseThrow(
-                () -> new BaseException(UserError.USER_NOT_FOUND)
+                () -> new BaseException(USER_NOT_FOUND)
         );
     }
 
     @Override
     public UserResponseDto findUser(Long userId) {
         User findUser = findByIdOrElseThrow(userId);
+        return UserResponseDto.from(findUser);
+    }
+
+    @Transactional
+    @Override
+    public UserResponseDto updateUser(long userId, UserUpdateRequestDto requestDto) {
+        if(userRepository.existsByPhoneNumber(requestDto.getPhoneNumber())){
+            throw new BaseException(DUPLICATE_PHONE_NUMBER);
+        }
+        User findUser = findByIdOrElseThrow(userId);
+
+        findUser.update(requestDto.getNickName(), requestDto.getPhoneNumber());
         return UserResponseDto.from(findUser);
     }
 
