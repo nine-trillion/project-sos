34709f6acc8f05864564e2a5079449c334359811
yigani <144660094+yigani@users.noreply.github.com>
2025-04-24 16:43:19 +0900

    test(menu)
    테스트 커버리지 확보

--- src/main/java/team/project/sos/domain/menu/dto/request/CreateMenuRequestDto.java
+++ src/main/java/team/project/sos/domain/menu/dto/request/CreateMenuRequestDto.java
@@ -1,12 +1,17 @@
-
 package team.project.sos.domain.menu.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreateMenuRequestDto {
 
     @NotNull(message = "가게 ID를 입력하세요.")
@@ -23,5 +28,4 @@ public class CreateMenuRequestDto {
 
     // TODO: [임시] 권한 체크용 필드, 추후 삭제 예정
     private String role;
-    
 }
