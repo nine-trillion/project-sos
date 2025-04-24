816ae82f4721e05998e1aaf7a7963a6839af0ae1
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-24 12:34:18 +0900

    feat(order): 선택한 회원의 주문 조회 로직 추가

--- src/main/java/team/project/sos/domain/order/controller/OrderController.java
+++ src/main/java/team/project/sos/domain/order/controller/OrderController.java
@@ -1,8 +1,8 @@
 package team.project.sos.domain.order.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import team.project.sos.domain.order.dto.request.CreateOrderRequestDto;
 import team.project.sos.domain.order.dto.response.OrderResponseDto;
@@ -18,11 +18,11 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/stores/{storeId}/order")
+    @PostMapping("/stores/{storeId}/orders")
     public ResponseEntity<Void> saveOrder(@RequestBody CreateOrderRequestDto requestDto,
-                                          HttpRequest request) {
-        // 유저 아이디 추출
-        Long userId = (Long) request.getAttributes().get("userId");
+                                          @AuthenticationPrincipal String currentUserId) {
+        // 토큰에서 로그인된 사용자 정보 꺼내기
+        Long userId = Long.parseLong(currentUserId);
 
         // 주문 생성
         Long orderId = orderService.saveOrder(requestDto, userId);
@@ -34,36 +34,42 @@ public class OrderController {
     }
 
     @PatchMapping("/orders/{orderId}")
-    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId, HttpRequest request) {
-        // 유저 아이디 추출
-//        Long userId = (Long) request.getAttributes().get("userId");
-
-        // TODO: 토큰에서 로그인된 사용자 정보 꺼내기
-        Long userId = 1L;
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId,
+                                            @AuthenticationPrincipal String currentUserId) {
+        // 토큰에서 로그인된 사용자 정보 꺼내기
+        Long userId = Long.parseLong(currentUserId);
 
         orderService.cancelOrder(orderId, userId);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/orders/{orderId}")
-    public ResponseEntity<OrderResponseDto> findOrder(@PathVariable Long orderId) {
-        // TODO: 토큰에서 로그인된 사용자 정보 꺼내기
-        Long userId = 1L;
+    public ResponseEntity<OrderResponseDto> findOrder(@PathVariable Long orderId,
+                                                      @AuthenticationPrincipal String currentUserId) {
+        // 토큰에서 로그인된 사용자 정보 꺼내기
+        Long userId = Long.parseLong(currentUserId);
 
         OrderResponseDto responseDto = orderService.findOrder(orderId, userId);
         return ResponseEntity.ok(responseDto);
     }
 
-//    @GetMapping("/orders")
-//    public ResponseEntity<List<OrderResponseDto>> findOrders(HttpRequest request) {
-//        // 유저 아이디 추출
-////        Long userId = (Long) request.getAttributes().get("userId");
-//
-//        // TODO: 토큰에서 로그인된 사용자 정보 꺼내기
-//        Long userId = 1L;
-//
-//        List<OrderResponseDto> responseDtos = orderService.findOrders(userId);
-//        return ResponseEntity.ok(responseDtos);
-//    }
+    /**
+     * 관리자가 사용자의 주문 목록을 조회하기 위해 사용합니다.
+     * 일반 사용자는 접근 권한이 없습니다.
+     */
+    @GetMapping("/users/{userId}/orders")
+    public ResponseEntity<List<OrderResponseDto>> findOrders(@PathVariable Long userId,
+                                                             @AuthenticationPrincipal String currentUserId) {
+
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponseDto>> findMyOrders(@AuthenticationPrincipal String currentUserId) {
+        // 토큰에서 로그인된 사용자 정보 꺼내기
+        Long userId = Long.parseLong(currentUserId);
+
+        List<OrderResponseDto> responseDtos = orderService.findMyOrders(userId);
+        return ResponseEntity.ok(responseDtos);
+    }
 
 }
