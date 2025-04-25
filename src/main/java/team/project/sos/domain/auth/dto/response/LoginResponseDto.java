556dd600a105949991404ca4f892743ff7a851f2
gganb <gksql0801@naver.com>
2025-04-25 16:48:16 +0900

    Feat/auth : 누락된 이메일 필드 추가

--- src/main/java/team/project/sos/domain/auth/dto/response/LoginResponseDto.java
+++ src/main/java/team/project/sos/domain/auth/dto/response/LoginResponseDto.java
@@ -8,22 +8,25 @@ import team.project.sos.domain.user.enums.UserRole;
 @Getter
 public class LoginResponseDto {
     private final Long userId;
+    private final String email;
     private final String nickName;
     private final UserRole role;
     private final Grade grade;
     private final String token;
 
-    public LoginResponseDto(Long userId, String nickName, UserRole role, Grade grade, String token) {
+    public LoginResponseDto(Long userId, String email, String nickName, UserRole role, Grade grade, String token) {
         this.userId = userId;
+        this.email = email;
         this.nickName = nickName;
         this.role = role;
         this.grade = grade;
         this.token = token;
     }
 
-    public static LoginResponseDto from(User user, String token){
+    public static LoginResponseDto from(User user, String token) {
         return new LoginResponseDto(
                 user.getId(),
+                user.getEmail(),
                 user.getNickname(),
                 user.getRole(),
                 user.getGrade(),
