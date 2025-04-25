1b0555d1cd25fffe81b4884c02bf358da908853b
gganb <gksql0801@naver.com>
2025-04-26 00:19:02 +0900

    Feat/user : 비밀번호 찾는 메서드 구현 - 요청받은 핸드폰 번호, 이메일이 존재하고 두 값이 매칭된다면 임시 비밀번호를 발급 - 임시비밀번호를 암호화해 데이터베이스에 업데이트

--- src/main/java/team/project/sos/domain/user/service/UserServiceImpl.java
+++ src/main/java/team/project/sos/domain/user/service/UserServiceImpl.java
@@ -1,26 +1,33 @@
 package team.project.sos.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.project.sos.common.excepion.BaseException;
 import team.project.sos.common.response.MessageResponse;
+import team.project.sos.domain.auth.dto.request.FindPasswordRequestDto;
+import team.project.sos.domain.auth.dto.response.FindPasswordResponseDto;
 import team.project.sos.domain.user.dto.request.UserPasswordRequestDto;
 import team.project.sos.domain.user.dto.request.UserUpdateRequestDto;
 import team.project.sos.domain.user.dto.response.UserResponseDto;
 import team.project.sos.domain.user.entity.User;
 import team.project.sos.domain.user.repository.UserRepository;
 import team.project.sos.domain.user.security.PasswordEncoder;
+import team.project.sos.domain.user.security.PasswordGenerator;
 
-import static team.project.sos.domain.auth.exception.AuthError.INVALID_PASSWORD;
+
+import static team.project.sos.domain.auth.exception.AuthError.*;
 import static team.project.sos.domain.user.exception.UserError.*;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final PasswordGenerator passwordGenerator;
 
     // userId 로 user 찾기
     @Override
@@ -36,8 +43,8 @@ public class UserServiceImpl implements UserService {
         return UserResponseDto.from(findUser);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public UserResponseDto updateUser(long userId, UserUpdateRequestDto requestDto) {
         if (userRepository.existsByPhoneNumber(requestDto.getPhoneNumber())) {
             throw new BaseException(DUPLICATE_PHONE_NUMBER);
@@ -58,13 +65,34 @@ public class UserServiceImpl implements UserService {
         return new MessageResponse("비밀번호가 확인되었습니다");
     }
 
-    @Transactional
+
     @Override
+    @Transactional
     public MessageResponse delete(Long userId) {
         User findUser = findByIdOrElseThrow(userId);
         findUser.deleteUser();
         return new MessageResponse("탈퇴되었습니다.");
     }
 
+    @Override
+    @Transactional
+    public FindPasswordResponseDto findPassword(FindPasswordRequestDto requestDto) {
+        User findUser = userRepository.findByPhoneNumber(requestDto.getPhoneNumber()).orElseThrow(
+                () -> new BaseException(USER_NOT_FOUND)
+        );
+        if (!findUser.getEmail().equals(requestDto.getEmail())) {
+            throw new BaseException(LOGIN_FAILED);
+        }
+        String tempPassword = passwordGenerator.generateTempPassword();
+
+        String encodedTempPwd = passwordEncoder.encode(tempPassword);
+
+        findUser.updatePassword(encodedTempPwd);
+        userRepository.save(findUser);
+        log.info("임시비밀번호(plain) = {}", tempPassword);
+        log.info("암호화된 비밀번호 = {}", encodedTempPwd);
+
+        return new FindPasswordResponseDto(tempPassword);
+    }
 
 }
