90859388d18132d464c1330cc6f9be059900ac69
gganb <gksql0801@naver.com>
2025-04-26 00:22:14 +0900

    Feat/user : 임시 비밀번호 생성 컴포넌트 - SRP에 따라 임시 비밀번호 생성 로직 분리 - Fisher-Yates Shuffle알고리즘을 적용해 요소를 랜덤으로 섞어 보안을 높힘 - 비밀번호 입력 포멧에 따라 8글자로 구성 - 대/소문자, 숫자, 특수문자 1개 이상 반드시 포함시킴

---
+++ src/main/java/team/project/sos/domain/user/security/PasswordGenerator.java
@@ -0,0 +1,44 @@
+package team.project.sos.domain.user.security;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class PasswordGenerator {
+    int length = 8;
+    private final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String lower = "abcdefghijklmnopqrstuvwxyz";
+    private final String numbers = "0123456789";
+    private final String special = "!@#$";
+    private final String all = upper + lower + numbers + special;
+
+    private final  Random random = new Random();
+
+    public String generateTempPassword() {
+
+        StringBuilder tempPwd = new StringBuilder();
+
+        tempPwd.append(upper.charAt(random.nextInt(upper.length())));
+        tempPwd.append(lower.charAt(random.nextInt(lower.length())));
+        tempPwd.append(numbers.charAt(random.nextInt(numbers.length())));
+        tempPwd.append(special.charAt(random.nextInt(special.length())));
+
+        for (int i = 0; i < length; i++) {
+            tempPwd.append(all.charAt(random.nextInt(all.length())));
+        }
+        char[] pwd = tempPwd.toString().toCharArray();
+
+        for (int i = pwd.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = pwd[i];
+            pwd[i] = pwd[j];
+            pwd[j] = temp;
+        }
+        StringBuilder resultPwd = new StringBuilder();
+        for (char c : pwd) {
+            resultPwd.append(c);
+        }
+        return resultPwd.toString();
+    }
+}
